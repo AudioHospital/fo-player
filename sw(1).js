1319bdc0e4da9385ebbeb5aea2f137a6e79a1368
Audio Hospital <audiohospital@tutamail.com>
2026-05-17 22:05:32 +0200

    Add files via upload

---
+++ sw(1).js
@@ -0,0 +1,26 @@
+const CACHE = 'fo-player-v3';
+const ASSETS = [
+  './FrequencyOasis_Player.html',
+  './manifest.json',
+  './fo-icon-192.png',
+  './fo-icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
